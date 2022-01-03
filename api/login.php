1f31efc40a8613d03d0c8135245a723d452be1c3
ijTuhin <isratishu0209@gmail.com>
2022-01-03 12:51:44 +0600

    new file

--- api/login.php
+++ api/login.php
@@ -2,14 +2,14 @@
     session_start();
     include("connect.php");
 
-    $mobile = $_POST['mobile'];
+    $s_id = $_POST['s_id'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
-    $role = $_POST['role'];
 
-    $check = mysqli_query($connect, "SELECT * FROM user WHERE mobile='$mobile' AND password='$password' AND role='$role'");
+    $check = mysqli_query($connect, "SELECT * FROM vote WHERE s_id='$s_id' AND email='$email' AND password='$password'");
     if(mysqli_num_rows($check) > 0){
       $userdata = mysqli_fetch_array($check);
-      $groups = mysqli_query($connect, "SELECT * FROM user WHERE role =2 ");
+      $groups = mysqli_query($connect, "SELECT * FROM vote WHERE role =2 ");
       $groupsdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
       $_SESSION['userdata'] = $userdata;
       $_SESSION['groupsdata'] = $groupsdata;
